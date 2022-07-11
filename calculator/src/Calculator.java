4c25e48dbea1760825049f80af73d6fb848042d6
IvanGit2021 <ivan.dias.moreira@gmail.com>
2022-07-11 15:06:11 +0100

    multiply
    add multiply method

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -2,5 +2,8 @@ public class Calculator {
     // sum -> filipe
     // subtract -> ana
     // multiply -> ivan
+    public int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
     // divide -> gonçalo
 }
