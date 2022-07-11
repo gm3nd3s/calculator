96b16be56814f3a3499603317587ee9611fcdcf7
AnaCDSequeira <acdsequeira@gmail.com>
2022-07-11 15:08:51 +0100

    subtract added

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -1,6 +1,10 @@
 public class Calculator {
     // sum -> filipe
     // subtract -> ana
+    public int subtract (int a, int b) {
+        return a - b;
+    }
     // multiply -> ivan
     // divide -> gonçalo
+
 }
