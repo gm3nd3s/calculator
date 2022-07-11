52adccc3a8924151e774255376b50669b5cf43ba
Gonçalo Mendes <goncalo.mendes@mindswap.academy>
2022-07-11 14:15:19 +0100

    Divide exercise done

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -3,4 +3,11 @@ public class Calculator {
     // subtract -> ana
     // multiply -> ivan
     // divide -> gonçalo
+    public float divide (float a, float b) {
+        if(a == 0 || b == 0){
+            return 0;
+        } else {
+            return a / b;
+        }
+    }
 }
--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -7,11 +7,11 @@ public class Main {
         int sum = calculator.sum(1,2);
         int subtract = calculator.subtract(4,3);
         int multiply = calculator.multiply(2,3);
-        int divide = calculator.divide(6,3);
+        float divide = calculator.divide( 6,3);
 
         System.out.println(sum) //should print 3
         System.out.println(subtract) //should print 1
         System.out.println(multiply) //should print 6
-        System.out.println() //should print 2
+        System.out.println(divide); //should print 2
     }
 }
